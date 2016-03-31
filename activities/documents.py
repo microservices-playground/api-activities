054e6c5c257c6e2f12f89bd858383b185f938a07
Karol Wnuk <k.wnuk@op.pl>
2016-03-31 22:56:52 +0100

    Documents updated

--- activities/documents.py
+++ activities/documents.py
@@ -8,24 +8,22 @@ class Activity(Document):
     config_polymorphic_collection = True
 
     type = StringField()
-    performed_by = StringField()
-    performer_avatar = StringField()
     created_at = DateTimeField()
     clicked = BoolField()
     user_id = IntField()
-
-    def __str__(self):
-        return '%s: activity by %s' % (self.created_at, self.performed_by)
 
 
 class Like(Activity):
     config_polymorphic_identity = 'like'
 
+    performed_by = StringField()
+    performer_avatar = StringField()
     post_id = IntField()
     post_image = StringField()
 
     def to_dict(self):
         return {
+            'id': self.mongo_id.__str__(),
             'type': self.type,
             'performed_by': self.performed_by,
             'performer_avatar': self.performer_avatar,
@@ -39,11 +37,14 @@ class Like(Activity):
 class Comment(Activity):
     config_polymorphic_identity = 'comment'
 
+    performed_by = StringField()
+    performer_avatar = StringField()
     post_id = IntField()
     post_image = StringField()
 
     def to_dict(self):
         return {
+            'id': self.mongo_id.__str__(),
             'type': self.type,
             'performed_by': self.performed_by,
             'performer_avatar': self.performer_avatar,
@@ -57,11 +58,14 @@ class Comment(Activity):
 class Repost(Activity):
     config_polymorphic_identity = 'repost'
 
+    performed_by = StringField()
+    performer_avatar = StringField()
     post_id = IntField()
     post_image = StringField()
 
     def to_dict(self):
         return {
+            'id': self.mongo_id.__str__(),
             'type': self.type,
             'performed_by': self.performed_by,
             'performer_avatar': self.performer_avatar,
@@ -75,8 +79,12 @@ class Repost(Activity):
 class Follow(Activity):
     config_polymorphic_identity = 'follow'
 
+    performed_by = StringField()
+    performer_avatar = StringField()
+
     def to_dict(self):
         return {
+            'id': self.mongo_id.__str__(),
             'type': self.type,
             'performed_by': self.performed_by,
             'performer_avatar': self.performer_avatar,
@@ -88,11 +96,14 @@ class Follow(Activity):
 class Mention(Activity):
     config_polymorphic_identity = 'mention'
 
+    performed_by = StringField()
+    performer_avatar = StringField()
     post_id = IntField()
     post_image = StringField()
 
     def to_dict(self):
         return {
+            'id': self.mongo_id.__str__(),
             'type': self.type,
             'performed_by': self.performed_by,
             'performer_avatar': self.performer_avatar,
@@ -108,13 +119,17 @@ class Badge(Activity):
 
     badge_name = StringField()
     badge_image = StringField()
+    post_id = IntField()
+    post_image = StringField()
 
     def to_dict(self):
         return {
+            'id': str(self.mongo_id),
             'type': self.type,
             'badge_name': self.badge_name,
             'badge_image': self.badge_image,
-            'post_id'
+            'post_id': self.post_id,
+            'post_image': self.post_image,
             'created_at': self.created_at.isoformat(),
             'clicked': self.clicked
         }
